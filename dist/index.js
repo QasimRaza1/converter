1acf99dc9fbaa57823a29fc3f13d52658b62e84c
QasimRaza1 <qali65846@gmail.com>
2024-06-23 16:27:14 +0500

    Project_Converter

---
+++ dist/index.js
@@ -0,0 +1,48 @@
+import inquirer from "inquirer";
+let Converter = {
+    "PKR": {
+        "USD": 0.004,
+        "GBP": 0.0037,
+        "PKR": 1
+    },
+    "GBP": {
+        "USD": 1.21,
+        "PKR": 271.79,
+        "GBP": 1
+    },
+    "USD": {
+        "PKR": 225.50,
+        "GBP": 0.83,
+        "USD": 1
+    }
+};
+async function convertCurrency() {
+    const answer = await inquirer.prompt([
+        {
+            type: "list",
+            name: "from",
+            choices: ["PKR", "USD", "GBP"],
+            message: "Select your currency:"
+        },
+        {
+            type: "list",
+            name: "to",
+            choices: ["PKR", "USD", "GBP"],
+            message: "Select target currency:"
+        },
+        {
+            type: "number",
+            name: "amount",
+            message: "Enter your conversion amount:"
+        }
+    ]);
+    const { from, to, amount } = answer;
+    if (from && to && amount) {
+        let result = Converter[from][to] * amount;
+        console.log(`Your conversion from ${from} to ${to} is ${result}`);
+    }
+    else {
+        console.log("Invalid inputs");
+    }
+}
+convertCurrency();
